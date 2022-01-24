22207d0a29aa2c6a5c928623c379cc464372085b
dumbus <dumbusx@gmail.com>
2022-01-24 23:49:26 +0300

    feat: add self-rating to console

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,3 +4,12 @@ import renderPage from './modules/renderPage';
 (async () => {
   await renderPage();
 })();
+
+console.log(`
+Score: 190/190;
+[+] Basic structure (25/25)
+[+] "Garage" view (50/50)
+[+] Car animation (50/50)
+[+] Race animation (30/30)
+[+] "Winners" view (35/35)
+`);
